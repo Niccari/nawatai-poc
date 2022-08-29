66adc0e2ea8f0ad5bcac048c62f27bbf92eaff8e
Niccari <latieos93@gmail.com>
2022-08-29 20:47:40 +0900

    remove: unused PendingImage model

--- src/models/pendingImage.ts
+++
@@ -1,9 +0,0 @@
-export type PendingImageWithoutId = {
-  authorId: string;
-  uploadedAt: Date;
-  isBinded: boolean;
-};
-
-export interface PendingImage extends PendingImageWithoutId {
-  id: string;
-}
